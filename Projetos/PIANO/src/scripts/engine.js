e41d163ce85f6201fdb2d62c03e464a8c095b9b4
Efraim Rocha <67542881+efraimrocha@users.noreply.github.com>
2023-12-18 02:19:43 -0300

    Adicionando sonso de cada tecla

--- Projetos/PIANO/src/scripts/engine.js
+++ Projetos/PIANO/src/scripts/engine.js
@@ -1,10 +1,17 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
-let audio = new Audio("tunes")
+let audio = new Audio("src/tunes/a.wav")
 
-const playTune = (key) => {};
+const playTune = (key) => {
+    audio.src = `src/tunes/${key}.wav`
+    audio.play();
+};
 
 pianoKeys.forEach((key) => {
     console.log(key.dataset.key);
-    key.addEventListener("click",()=> playtune(key.dataset.key))
+    key.addEventListener("click",()=> playTune(key.dataset.key))
+})
+
+document,addEventListener("keydown", (e) => {
+    playTune(e.key);
 })
