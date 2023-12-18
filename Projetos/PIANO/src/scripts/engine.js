1fb2a6f59309811af9f5bc130aca79e776e05357
Efraim Rocha <67542881+efraimrocha@users.noreply.github.com>
2023-12-18 03:17:25 -0300

    Correção de erros

--- Projetos/PIANO/src/scripts/engine.js
+++ Projetos/PIANO/src/scripts/engine.js
@@ -6,36 +6,35 @@ let mapedKeys = [];
 let audio = new Audio("src/tunes/a.wav");
 
 const playTune = (key) => {
-    audio.src = `src/tunes/${key}.wav`
-    audio.play();
+  audio.src = `src/tunes/${key}.wav`;
+  audio.play();
 
-
-    const clickedKey = document.querySelector(`[data-key="${key}"]`)
-    clickedKey.classList.add("active");
-    setTimeout(()=> {
-        clickedKey.classList.remove("active");
-    }, 150);
+  const clickedKey = document.querySelector(`[data-key="${key}"]`);
+  clickedKey.classList.add("active");
+  setTimeout(() => {
+    clickedKey.classList.remove("active");
+  }, 150);
 };
 
 pianoKeys.forEach((key) => {
-    key.addEventListener("click",()=> playTune(key.dataset.key));
-    mapedKeys.push(key.dataset.key);
+  key.addEventListener("click", () => playTune(key.dataset.key));
+  mapedKeys.push(key.dataset.key);
 });
 
-document,addEventListener("keydown", (e) => {
-    if (mapedKeys.includes(e.key)){
-        playTune(e.key); 
-    };
+document.addEventListener("keydown", (e) => {
+  if (mapedKeys.includes(e.key)) {
+    playTune(e.key);
+  }
 });
 
-const handleVolume = (e) =>{
-    audio.volume = e.target.value;    
-}
+const handleVolume = (e) => {
+  audio.volume = e.target.value;
+};
 
 const showHideKeys = () => {
-    pianoKeys.forEach(key => key.classList.toggle("hide"))
-}
+  pianoKeys.forEach((key) => key.classList.toggle("hide"));
+};
 
-volumeSlider.addEventListener("input", handleVolume)
+volumeSlider.addEventListener("input", handleVolume);
 
-keysCheck.addEventListener("click", showHideKeys)
+keysCheck.addEventListener("click", showHideKeys);
